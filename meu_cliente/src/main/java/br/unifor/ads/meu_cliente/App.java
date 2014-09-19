6994b6755f75eb41edd2c2e477b9d9be6daed622
Anderson Mendes <andersonmpm@gmail.com>
2014-09-19 20:17:46 -0300

    [esmenia-borba] iniciar

---
+++ meu_cliente/src/main/java/br/unifor/ads/meu_cliente/App.java
@@ -0,0 +1,13 @@
+package br.unifor.ads.meu_cliente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
